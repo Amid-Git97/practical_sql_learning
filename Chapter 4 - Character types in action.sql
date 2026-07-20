a16648ab419fb63b374f8e08898a9d66a4b847c3
Amid-Git97 <amid-kamara@outlook.com>
2026-07-20 23:40:22 +0100

    Chapter 4 - Understanding Data Types

--- Chapter 4 - Character types in action.sql
+++ Chapter 4 - Character types in action.sql
@@ -2,3 +2,17 @@ INSERT INTO char_data_types
 VALUES
     ('abc', 'abc', 'abc'),
     ('defghi', 'defghi', 'defghi');
+
+-- Creating a table using 2 different auto-incrementing integers
+
+-- Auto-incrementing with SERIAL
+CREATE TABLE people(
+id serial,
+person_name varchar(100)
+);
+
+-- Auto incrementing with IDENTITY
+ CREATE TABLE people (
+id integer GENERATED ALWAYS AS IDENTITY, 
+person_name varchar(100)
+);
