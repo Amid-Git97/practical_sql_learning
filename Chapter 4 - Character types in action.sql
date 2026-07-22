c51c9adcaf8f07c6ad2b997cc7e54524923df484
Amid-Git97 <amid-kamara@outlook.com>
2026-07-22 23:27:51 +0100

    Chapter 4 - Understanding Data Types

--- Chapter 4 - Character types in action.sql
+++ Chapter 4 - Character types in action.sql
@@ -16,3 +16,26 @@ person_name varchar(100)
 id integer GENERATED ALWAYS AS IDENTITY, 
 person_name varchar(100)
 );
+
+
+-- Listing 4-3: Rounding issues with float columns
+-- Assumes table created and loaded with Listing 4-2
+
+SELECT
+    numeric_column * 10000000 AS fixed,
+    real_column * 10000000 AS floating
+FROM number_data_types
+WHERE numeric_column = .7;
+
+-- Listing 4-4: The timestamp and interval types in action
+
+CREATE TABLE date_time_types (
+    timestamp_column timestamp with time zone,
+    interval_column interval
+);
+
+INSERT INTO date_time_types
+VALUES
+    ('2022-12-31 01:00 EST','2 days'),
+    ('2022-12-31 01:00 -8','1 month'),
+    ('2022-12-31 01:00 Australia/Melbourne','1 century'),
