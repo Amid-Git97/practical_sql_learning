9329807d050b0436dd0f3e0891796556af9894d7
Amid-Git97 <amid-kamara@outlook.com>
2026-07-23 23:25:43 +0100

    Chapter 4 - Understanding Data Type

--- Chapter 4 - Character types in action.sql
+++ Chapter 4 - Character types in action.sql
@@ -39,3 +39,11 @@ VALUES
     ('2022-12-31 01:00 EST','2 days'),
     ('2022-12-31 01:00 -8','1 month'),
     ('2022-12-31 01:00 Australia/Melbourne','1 century'),
+
+    -- Using Interval Data Types in Calculations
+    
+    SELECT 
+	timestamp_column,
+	interval_column,
+	timestamp_column - interval_column AS new_date
+FROM date_time_types;
