764eccb78f81d68bc05edad095f269ce1949c1fc
Amid-Git97 <amid-kamara@outlook.com>
2026-08-06 23:48:41 +0100

    Chapter 5 - Importing and Exporting Data

--- Chapter 5 - Importing and Exporting Data.sql
+++ Chapter 5 - Importing and Exporting Data.sql
@@ -58,3 +58,22 @@ CREATE TABLE supervisor_salaries (
 COPY supervisor_salaries (town, supervisor, salary)
 FROM 'C:\YourDirectory\supervisor_salaries.csv'
 WITH (FORMAT CSV, HEADER);
+
+
+-- Listing 5-7: Using a temporary table to add a default value to a column during
+-- import
+
+DELETE FROM supervisor_salaries;
+
+CREATE TEMPORARY TABLE supervisor_salaries_temp 
+    (LIKE supervisor_salaries INCLUDING ALL);
+
+COPY supervisor_salaries_temp (town, supervisor, salary)
+FROM 'C:\YourDirectory\supervisor_salaries.csv'
+WITH (FORMAT CSV, HEADER);
+
+INSERT INTO supervisor_salaries (town, county, supervisor, salary)
+SELECT town, 'Mills', supervisor, salary
+FROM supervisor_salaries_temp;
+
+DROP TABLE supervisor_salaries_temp;
