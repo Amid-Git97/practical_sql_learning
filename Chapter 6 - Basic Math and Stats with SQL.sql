1b0f846c7863ad4c8097a0ae485c7ff97cb71304
Amid-Git97 <amid-kamara@outlook.com>
2026-08-20 18:16:29 +0100

    Chapter 6 - Basic Math and Stats with SQL

--- Chapter 6 - Basic Math and Stats with SQL.sql
+++ Chapter 6 - Basic Math and Stats with SQL.sql
@@ -12,3 +12,13 @@ SELECT 11 / 6;   -- integer division
 SELECT 11 % 6;   -- modulo division
 SELECT 11.0 / 6; -- decimal division
 SELECT CAST(11 AS numeric(3,1)) / 6;
+
+
+-- Listing 6-3: Exponents, roots and factorials with SQL
+
+SELECT 3 ^ 4;         -- exponentiation
+SELECT |/ 10;         -- square root (operator)
+SELECT sqrt(10);      -- square root (function)
+SELECT ||/ 10;        -- cube root
+SELECT factorial(4);  -- factorial (function)
+SELECT 4 !;           -- factorial (operator; PostgreSQL 13 and earlier only)
