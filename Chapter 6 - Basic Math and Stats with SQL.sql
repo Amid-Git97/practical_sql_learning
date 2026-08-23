d9b19420f72341e0de0f5615deb9095900f3c906
Amid-Git97 <amid-kamara@outlook.com>
2026-08-23 22:53:09 +0100

    Chapter 6 - Basic Math and Stats with SQL

--- Chapter 6 - Basic Math and Stats with SQL.sql
+++ Chapter 6 - Basic Math and Stats with SQL.sql
@@ -22,3 +22,12 @@ SELECT sqrt(10);      -- square root (function)
 SELECT ||/ 10;        -- cube root
 SELECT factorial(4);  -- factorial (function)
 SELECT 4 !;           -- factorial (operator; PostgreSQL 13 and earlier only)
+
+
+-- Order of operations
+
+SELECT 7 + 8 * 9; 	-- answer: 79
+SELECT (7 + 8) * 9;	-- answer: 135
+
+SELECT 3 ^ 3 - 1;   -- answer: 26
+SELECT 3 ^ (3 - 1); -- answer: 9
