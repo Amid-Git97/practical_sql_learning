4adaf4c0b186c9b6e2fb4657e6cd8977133aa907
Amid-Git97 <amid-kamara@outlook.com>
2026-09-07 15:26:11 +0100

    Chapter 6 - Basic Math and Stats with SQL

--- Chapter 6 - Basic Math and Stats with SQL.sql
+++ Chapter 6 - Basic Math and Stats with SQL.sql
@@ -84,3 +84,28 @@ SELECT county_name AS county,
 	CAST(CAST(area_water AS numeric) / (area_land + area_water)  * 100 AS numeric(6,2)) AS pct_water
 FROM us_counties_pop_est_2019
 ORDER BY pct_water DESC;
+
+
+-- Listing 6-8: Calculating percent change
+
+CREATE TABLE percent_change (
+    department text,
+    spend_2019 numeric(10,2),
+    spend_2022 numeric(10,2)
+);
+
+INSERT INTO percent_change
+VALUES
+    ('Assessor', 178556, 179500),
+    ('Building', 250000, 289000),
+    ('Clerk', 451980, 650000),
+    ('Library', 87777, 90001),
+    ('Parks', 250000, 223000),
+    ('Water', 199000, 195000);
+
+SELECT department,
+       spend_2019,
+       spend_2022,
+       round( (spend_2022 - spend_2019) /
+                    spend_2019 * 100, 1) AS pct_change
+FROM percent_change;
