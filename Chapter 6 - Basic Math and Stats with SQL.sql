49b2fd82b84e0c85a4b1c340e118007a84cca02c
Amid-Git97 <amid-kamara@outlook.com>
2026-09-09 22:09:00 +0100

    Chapter 6 - Basic Math and Stats with SQL

--- Chapter 6 - Basic Math and Stats with SQL.sql
+++ Chapter 6 - Basic Math and Stats with SQL.sql
@@ -131,3 +131,13 @@ SELECT
     percentile_disc(.5)
     WITHIN GROUP (ORDER BY numbers)
 FROM percentile_test;
+
+
+
+-- Listing 6-11: Using sum(), avg(), and percentile_cont() aggregate functions
+
+SELECT sum(pop_est_2019) AS county_sum,
+       round(avg(pop_est_2019), 0) AS county_average,
+       percentile_cont(.5)
+       WITHIN GROUP (ORDER BY pop_est_2019) AS county_median
+FROM us_counties_pop_est_2019;
