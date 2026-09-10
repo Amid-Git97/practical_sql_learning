76e9df5afdad6477c5ac380337320226cc80995d
Amid-Git97 <amid-kamara@outlook.com>
2026-09-10 23:05:33 +0100

    Chapter 6 - Basic Math and Stats with SQL

--- Chapter 6 - Basic Math and Stats with SQL.sql
+++ Chapter 6 - Basic Math and Stats with SQL.sql
@@ -141,3 +141,26 @@ SELECT sum(pop_est_2019) AS county_sum,
        percentile_cont(.5)
        WITHIN GROUP (ORDER BY pop_est_2019) AS county_median
 FROM us_counties_pop_est_2019;
+
+
+-- Listing 6-12: Passing an array of values to percentile_cont()
+
+-- quartiles
+SELECT percentile_cont(ARRAY[.25,.5,.75])
+       WITHIN GROUP (ORDER BY pop_est_2019) AS quartiles
+FROM us_counties_pop_est_2019;
+
+-- Listing 6-12: Passing an array of values to percentile_cont()
+
+-- quartiles
+SELECT percentile_cont(ARRAY[.25,.5,.75])
+       WITHIN GROUP (ORDER BY pop_est_2019) AS quartiles
+FROM us_counties_pop_est_2019;
+
+-- Listing 6-13: Using unnest() to turn an array into rows
+
+SELECT unnest(
+            percentile_cont(ARRAY[.25,.5,.75])
+            WITHIN GROUP (ORDER BY pop_est_2019)
+            ) AS quartiles
+FROM us_counties_pop_est_2019;
