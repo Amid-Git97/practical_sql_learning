4f481508fe5b3a28979d2956b212c6feccee81fb
Amid-Git97 <amid-kamara@outlook.com>
2026-09-25 23:35:47 +0100

    Chapter 7 - Joining Tables In A Relational Database

--- Chapter 7 - Joining Tables In A Relational Database.sql
+++ Chapter 7 - Joining Tables In A Relational Database.sql
@@ -111,3 +111,18 @@ ORDER BY district_2020.id;
 SELECT *
 FROM district_2020 CROSS JOIN district_2035
 ORDER BY district_2020.id, district_2035.id;
+
+
+
+-- Listing 7-10: Filtering to show missing values with IS NULL
+
+SELECT *
+FROM district_2020 LEFT JOIN district_2035
+ON district_2020.id = district_2035.id
+WHERE district_2035.id IS NULL;
+
+-- alternately, with a RIGHT JOIN
+SELECT *
+FROM district_2020 RIGHT JOIN district_2035
+ON district_2020.id = district_2035.id
+WHERE district_2020.id IS NULL;
